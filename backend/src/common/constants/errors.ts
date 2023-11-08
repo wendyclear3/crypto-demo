0e2cfa5cac01927eb7b7a767717ae6be81e020ad
Elena Anosova <anosova.helena3@gmail.com>
2023-11-08 21:02:01 +0100

    implemented login logic

---
+++ backend/src/common/constants/errors.ts
@@ -0,0 +1,5 @@
+export const AppError = {
+  USER_EXIST: 'User with this Email already exist',
+  USER_NOT_EXIST: 'User with this email does not exist',
+  WRONG_DATA: 'Wrong data',
+};
---
+++ backend/src/modules/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDTO } from '../users/dto';
+import { UserLoginDTO } from './dto';
+import { AuthUserResponse } from './response';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() dto: CreateUserDTO): Promise<CreateUserDTO> {
+    return this.authService.registerUsers(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
+    return this.authService.loginUser(dto);
+  }
+}
