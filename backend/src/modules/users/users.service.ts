f2153956be3399864893224d0b1f95820574ebe3
Elena Anosova <anosova.helena3@gmail.com>
2023-11-24 23:32:42 +0100

    added try/catch in services

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -11,43 +11,74 @@ export class UserService {
     @InjectModel(User) private readonly userRepository: typeof User,
   ) {}
 
-  async hashPassword(password) {
-    return bcrypt.hash(password, 10); //два параметра - пароль, который надо захэшировать и солт, модификатор с помощью которого хэш будет всегда разным
+  async hashPassword(password: string): Promise<string> {
+    try {
+      return bcrypt.hash(password, 10); //два параметра - пароль, который надо захэшировать и солт, модификатор с помощью которого хэш будет всегда разным
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 
-  async findUserByEmail(email: string) {
-    return this.userRepository.findOne({ where: { email } }); //findOne - находим что-то одно и обращаемся к базе данных, передавая набор опций, по которым мы можем производить поиск
+  async findUserByEmail(email: string): Promise<User> {
+    try {
+      return this.userRepository.findOne({
+        //findOne - находим что-то одно и обращаемся к базе данных, передавая набор опций, по которым мы можем производить поиск
+        where: { email: email },
+        include: {
+          model: Watchlist,
+          required: false,
+        },
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
-    dto.password = await this.hashPassword(dto.password); //обратились к дататрансферобжект к полю "пароль" и сказали что он теперь равняется результату выполнения метода hashPassword и именно ею мы заменяем наш строковый пароль который нам пришел.
-    await this.userRepository.create({
-      firstName: dto.firstName,
-      userName: dto.userName,
-      email: dto.email,
-      password: dto.password,
-    });
-    return dto;
+    try {
+      dto.password = await this.hashPassword(dto.password); //обратились к дататрансферобжект к полю "пароль" и сказали что он теперь равняется результату выполнения метода hashPassword и именно ею мы заменяем наш строковый пароль который нам пришел.
+      await this.userRepository.create({
+        firstName: dto.firstName,
+        userName: dto.userName,
+        email: dto.email,
+        password: dto.password,
+      });
+      return dto;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  async publicUser(email: string) {
-    return this.userRepository.findOne({
-      where: { email: email },
-      attributes: { exclude: ['password'] },
-      include: {
-        model: Watchlist,
-        required: false,
-      },
-    });
+  async publicUser(email: string): Promise<User> {
+    try {
+      return this.userRepository.findOne({
+        where: { email: email },
+        attributes: { exclude: ['password'] },
+        include: {
+          model: Watchlist,
+          required: false,
+        },
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async updateUser(email: string, dto: UpdateUserDTO): Promise<UpdateUserDTO> {
-    await this.userRepository.update(dto, { where: { email } });
-    return dto;
+    try {
+      await this.userRepository.update(dto, { where: { email } });
+      return dto;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async deleteUser(email: string): Promise<boolean> {
-    await this.userRepository.destroy({ where: { email } });
-    return true;
+    try {
+      await this.userRepository.destroy({ where: { email } });
+      return true;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
