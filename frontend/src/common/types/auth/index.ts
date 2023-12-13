9776a25dd8d2538e393bafab080b1147f8415948
Elena Anosova <anosova.helena3@gmail.com>
2023-12-13 01:11:46 +0100

    eliminated little bugs

--- frontend/src/common/types/auth/index.ts
+++ frontend/src/common/types/auth/index.ts
@@ -1,6 +1,7 @@
 export interface IPropsLogin {
   setPassword: (value: string) => void
   setEmail: (value: string) => void
+  navigate: (to: string) => void
 }
 
 export interface IPropsRegister {
@@ -9,4 +10,29 @@ export interface IPropsRegister {
   setRepeatPassword: (value: string) => void
   setFirstName: (value: string) => void
   setUserName: (value: string) => void
+  navigate: (to: string) => void
+}
+
+export interface IAuthState {
+  user: iPublicUser
+  isLogged: boolean
+}
+
+export interface iPublicUser {
+  id: number | null
+  firstName: string
+  userName: string
+  email: string
+  createdAt: string
+  updatedAt: string
+  watchList: [IWatchList]
+}
+
+export interface IWatchList {
+  id: number | null
+  name: string
+  assetId: string
+  createdAt: string
+  updatedAt: string
+  user: number | null
 }
