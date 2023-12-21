bd172eaa8f917a85a9896eda026bb0ea9da4b9ea
unknown <anosova.helena3@gmail.com>
2023-12-21 22:24:25 +0100

    added layout of sidebar using mui

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { ILayout } from "../../common/types/layout";
 import TopBarComponent from "../top-bar";
+import { useLocation } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
+import SidebarComponent from "../sidebar";
 
 const LayoutComponent = ({ children }: ILayout) => {
-  return (
-    <>
-      <TopBarComponent />
-      {children}
-    </>
+  const [isOpen, setIsOpen] = useState(true);
+  const location = useLocation();
+  const isNoneMobile = useMediaQuery("(min-width: 600px)");
+  return location.pathname === "/login" || location.pathname === "/register" ? (
+    <>{children} </>
+  ) : (
+    <Box display={isNoneMobile ? "flex" : "block"} width="100%" height="100%">
+      <SidebarComponent
+        isNoneMobile={isNoneMobile}
+        drawerWidth="250"
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+      <Box>
+        <TopBarComponent />
+        {children}
+      </Box>
+    </Box>
   );
 };
 
