81aa2f8af6e29945ed6df5b957e0f474496370d8
Elena Anosova <anosova.helena3@gmail.com>
2024-01-17 21:48:26 +0100

    fetching data from coingecko: created fetch logic, saved data in store, getting data from store in homepage

---
+++ frontend/src/pages/home/styles.ts
@@ -0,0 +1,50 @@
+import { makeStyles } from '@mui/styles'
+import { tokens } from '../../theme'
+import { Theme } from '@mui/material'
+
+export const useStyles = makeStyles((theme: Theme) => {
+  const colors = tokens(theme.palette.mode)
+  return {
+    root: {
+      flexGrow: 1,
+      padding: '32px',
+    },
+    topCardItem: {
+      backgroundColor: `${
+        theme.palette.mode === 'light'
+          ? colors.primary.DEFAULT
+          : colors.primary[600]
+      }`,
+      padding: '10px 16px 20px 16px',
+      minHeight: 185,
+      border: `1px solid ${colors.borderColor}`,
+      borderRadius: 12,
+    },
+    assetName: {
+      fontSize: 25,
+      fontWeight: 600,
+      lineHeight: '30px',
+      textTransform: 'capitalize',
+    },
+    itemDetails: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+      paddingBottom: '20px',
+    },
+    cardPrice: {
+      fontSize: 32,
+      fontWeight: 700,
+      lineHeight: '48px',
+      margin: '0',
+    },
+    cardCapitalize: {
+      color: `${colors.secondary.DEFAULT}`,
+      fontWeight: 400,
+      fontSize: 18,
+      lineHeight: '21px',
+      margin: '0',
+    },
+  }
+})
