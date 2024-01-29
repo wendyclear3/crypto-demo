1db061b127016b2ddc2aaf957ec5d69962d4c73b
Elena Anosova <anosova.helena3@gmail.com>
2024-01-30 00:45:54 +0100

    fix

--- frontend/src/components/charts/line-chart/index.tsx
+++ frontend/src/components/charts/line-chart/index.tsx
@@ -49,7 +49,7 @@ const LineChart = (props: ILineChartProps) => {
     ),
     datasets: [
       {
-        label: data[0].name.chartAt(0).toUpperCase() + data[0].name.slice(1),
+        label: data[0].name.charAt(0).toUpperCase() + data[0].name.slice(1),
         data: data[0].price_chart_data.map((element: any) => element[1]),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
